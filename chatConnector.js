f05e1cedcbeaeaf803942bcace2f2589c39256ce
Ville Anttonen <ville.anttonen@gmail.com>
2013-10-16 19:37:56 +0300

    this and that

--- chatConnector.js
+++ chatConnector.js
@@ -1,17 +1,28 @@
-  var ChatConnector = function(url,name){
+  var ChatConnector = function(url){
   	var socket = io.connect(url);
-  	console.log("logging to chat..");
-  	socket.emit("login",name);
-  	
   	this.socket = socket;
   };
 
+  ChatConnector.prototype.login = function (name){
+   this.socket.emit("login",name,function (err){
+      throw err;
+   });
+  }
+
   ChatConnector.prototype.handleRooms = function(fn){
   	this.socket.on("updateRooms",fn);
-  }
+  };
+
+  ChatConnector.prototype.handleUserRooms = function(fn){
+    this.socket.on("updateUserRooms",fn);
+  };
 
   ChatConnector.prototype.handleMessages = function(fn){
   	this.socket.on("updateMessages",fn);
+  };
+
+  ChatConnector.prototype.handleRoomUsers= function(fn){
+    this.socket.on("updateUsersInRoom",fn);
   };
 
   ChatConnector.prototype.message = function (room,message){
@@ -24,5 +35,8 @@
   };
 
   ChatConnector.prototype.join = function (room){
-  	this.socket.emit("join",room);
-  }
+  	this.socket.emit("join",room,function (err){
+      throw err;
+    });
+  };
+
